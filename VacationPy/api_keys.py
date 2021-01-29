78e3c1346cad1b80da9bccf7a1f63de473aebe3d
poonam-ux <shaonam@gmail.com>
2021-01-29 14:41:49 -0800

    WeatherPy script

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "0bb6c0e7d7ee08f9a790856fb8f0db5f"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAMYOq-V-YbYt-p0LjVRcPWwJ-9b7lSozY"
